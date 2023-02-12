b4200e1313a1d94a537f87442b7850b419fb265c
PEDRO <pedropinacolider@gmail.com>
2023-02-12 16:18:50 -0400

    primeiro Commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="block">
+    <div class="todoarea box  is-flex is-flex-direction-column m-6 p-5">
+      <input class="input pd-6 mb-5" type="text" name="inputTarefa" id="inputTarefa" v-model="inputTarefa">
+    <button @click="adicionaTarefa" class="button is-primary">Adicionar</button>
+  </div>
+  <div id="arealista" class="" >
+    <ul v-for="(tarefa, ind) in tarefas" :key="tarefa">
+      <li class="message m-6 is-flex is-flex-direction-column">
+        
+        <span class="p-5">{{ tarefa }}</span>
+       <button class="button is-danger" @click="deletaTarefa(ind)">
+        Feito
+       </button>
+      </li>
+    </ul>
+  </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    name: 'App',
+    data(){
+      return{
+      inputTarefa:'',
+      tarefas:[]}
+    },
+    mounted(){
+      if(localStorage.getItem("tarefas")){
+      this.tarefas = JSON.parse(localStorage.getItem("tarefas"));
+      }
+    },
+    methods:{
+      adicionaTarefa(){
+        this.tarefas.push(this.inputTarefa);
+        this.salvaTarefas();
+      },
+      deletaTarefa(x){
+        this.tarefas.splice(x,1);
+        this.salvaTarefas;
+  
+      },
+      salvaTarefas(){
+        var parsed = JSON.stringify(this.tarefas);
+        localStorage.setItem("tarefas",parsed);
+      }
+    }
+  
   }
-}
-</script>
-
+  </script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
